9a6c35945e5fa12036bbcffcb9ee48c1ff78537a
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-04-25 17:58:45 +0200

    Stage 1/5: Welcome to the battlefield!

---
+++ src/tictactoe/Main.java
@@ -0,0 +1,13 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        displayBoard();
+    }
+
+    private static void displayBoard() {
+        System.out.println("X O X");
+        System.out.println("X O X");
+        System.out.println("X O X");
+    }
+}
