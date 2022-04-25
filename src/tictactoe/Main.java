a7bbbda45f846cbc35ce67e1c96902429619ff38
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-04-25 20:37:35 +0200

    Stage 2/5: The user is the gamemaster

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,13 +1,34 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        displayBoard();
+        init();
+        String userStr = userInput();
+        displayBoard(userStr);
     }
 
-    private static void displayBoard() {
-        System.out.println("X O X");
-        System.out.println("X O X");
-        System.out.println("X O X");
+    private static void init() {
+        System.out.print("Enter cells: ");
     }
+
+    private static String userInput() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+
+
+    private static void displayBoard(String s) {
+        System.out.println("---------");
+        System.out.printf("| %s %s %s |%n", s.charAt(0), s.charAt(1), s.charAt(2));
+        System.out.printf("| %s %s %s |%n", s.charAt(3), s.charAt(4), s.charAt(5));
+        System.out.printf("| %s %s %s |%n", s.charAt(6), s.charAt(7), s.charAt(8));
+        System.out.println("---------");
+    }
+
+
+
 }
