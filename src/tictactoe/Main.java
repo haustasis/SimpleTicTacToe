b82790dd0579fd318dd8c925f8e06e826a7ec0d3
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-05-17 17:17:59 +0200

    Stage 3/5: What's up on the field?

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,34 +1,64 @@
 package tictactoe;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        init();
-        String userStr = userInput();
-        displayBoard(userStr);
-    }
-
-    private static void init() {
-        System.out.print("Enter cells: ");
-    }
-
-    private static String userInput() {
         Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
-    }
 
+        System.out.print("Enter cells: ");
+        char[] xo = sc.nextLine().toCharArray();
+        char[][] field = new char[3][3];
 
+        int xs = 0;
+        int os = 0;
+        Boolean xxx = false;
+        Boolean ooo = false;
 
-    private static void displayBoard(String s) {
+        // fill up the field, count X's & O's
+        for (int i = 0; i < 9; i++) {
+            field[i/3][i%3] = xo[i];
+
+            if (xo[i] == 'X') {
+                xs += 1;
+            } else if (xo[i] == 'O') {
+                os += 1;
+            }
+        }
+
+        // check if row or column or any of diagonals is win for someone
+        for (int i = 0; i < 3; i++) {
+            int row = 0;
+            int clm = 0;
+            int mDiag = 0;
+            int aDiag = 0;
+
+            for (int j = 0; j < 3; j++) {
+                row += field[i][j];
+                clm += field[j][i];
+                mDiag += field[j][j];
+                aDiag += field[j][2-j];
+            }
+
+            // ASCII value for X is 88 (X+X+X is 264)
+            // ASCII value for O is 79 (O+O+O is 237)
+            xxx = xxx || row == 264 || clm == 264 || mDiag == 264 || aDiag == 264;
+            ooo = ooo || row == 237 || clm == 237 || mDiag == 237 || aDiag == 237;
+        }
+
+        // print field and result
         System.out.println("---------");
-        System.out.printf("| %s %s %s |%n", s.charAt(0), s.charAt(1), s.charAt(2));
-        System.out.printf("| %s %s %s |%n", s.charAt(3), s.charAt(4), s.charAt(5));
-        System.out.printf("| %s %s %s |%n", s.charAt(6), s.charAt(7), s.charAt(8));
+        for (int i = 0; i < 3; i++) {
+            System.out.println(String.format("| %c %c %c |", field[i][0], field[i][1], field[i][2]));
+        }
         System.out.println("---------");
+
+        String result = Math.abs(xs-os) > 1 || xxx && ooo ? "Impossible"
+                : xxx ? "X wins"
+                : ooo ? "O wins"
+                : xs + os == 9 ? "Draw"
+                : "Game not finished";
+
+        System.out.println(result);
     }
-
-
-
 }
